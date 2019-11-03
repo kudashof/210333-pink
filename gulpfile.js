abd5ac461ce97820dc2d4439ccf45383eeab8d60
AlexKu <kudashof@gmail.com>
2019-11-03 17:54:55 +0300

    сборка Less на Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,27 @@
 "use strict";
 
 var gulp = require("gulp");
-var less = require("gulp-less");
 var plumber = require("gulp-plumber");
+var sourcemap = require("gulp-sourcemaps");
+var less = require("gulp-less");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
 
-gulp.task("style", function() {
-  gulp.src("source/less/style.less")
+gulp.task("css", function () {
+  return gulp.src("source/less/style.less")
     .pipe(plumber())
+    .pipe(sourcemap.init())
     .pipe(less())
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(sourcemap.write("."))
     .pipe(gulp.dest("source/css"))
     .pipe(server.stream());
 });
 
-gulp.task("serve", ["style"], function() {
+gulp.task("server", function () {
   server.init({
     server: "source/",
     notify: false,
@@ -27,6 +30,8 @@ gulp.task("serve", ["style"], function() {
     ui: false
   });
 
-  gulp.watch("source/less/**/*.less", ["style"]);
+  gulp.watch("source/less/**/*.less", gulp.series("css"));
   gulp.watch("source/*.html").on("change", server.reload);
 });
+
+gulp.task("start", gulp.series("css", "server"));
