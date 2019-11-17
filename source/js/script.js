8035018e0fd66cf29f4cad813f07c575588a3a61
AlexKu <kudashof@gmail.com>
2019-11-17 16:59:02 +0300

    переносит js в файл

---
+++ source/js/script.js
@@ -0,0 +1,24 @@
+var navMain = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.main-nav__toggle');
+var siteList = document.querySelector('.main-nav__site-list');
+var pageHeader = document.querySelector('.page-header');
+
+navMain.classList.remove('main-nav--nojs');
+navToggle.classList.remove('main-nav__toggle--hidden');
+siteList.classList.add('main-nav__site-list--hidden');
+pageHeader.classList.add('page-header');
+pageHeader.classList.remove('page-header--nojs');
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    siteList.classList.remove('main-nav__site-list--hidden');
+    navMain.classList.add('main-nav--opened');
+    pageHeader.classList.add('page-header--menu-opened')
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+    siteList.classList.add('main-nav__site-list--hidden');
+    pageHeader.classList.remove('page-header--menu-opened')
+  }
+});
